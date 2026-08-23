8d17a6860c3b9ae4ee52d0be7c90ef863621c548
leebro7 <lwb29@qq.com>
2026-08-23 21:56:34 +0800

    fix(android): 移除不存在的 defaultWeight import（RowScope 成员无需 import）

--- composeApp/src/androidMain/kotlin/com/qust/helper/ui/appwidget/DailyLessonWidget.kt
+++ composeApp/src/androidMain/kotlin/com/qust/helper/ui/appwidget/DailyLessonWidget.kt
@@ -17,7 +17,6 @@ import androidx.glance.appwidget.provideContent
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Column
 import androidx.glance.layout.Row
-import androidx.glance.layout.defaultWeight
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.fillMaxWidth
 import androidx.glance.layout.padding
